5a3755d6374ad1049876a64ab3b5416d4dd3b7b1
Apil Khadka <apilkhadka2020@gmail.com>
2025-04-10 07:56:46 +0545

    Initial commit

---
+++ src/main/java/com/apil/java_blog_manager/JavaBlogManagerApplication.java
@@ -0,0 +1,13 @@
+package com.apil.java_blog_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaBlogManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaBlogManagerApplication.class, args);
+    }
+
+}
